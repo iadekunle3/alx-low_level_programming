5122ae524685f52d78d8356db6e2b89494499cff
iadekunle3 <iadekunle34@gmail.com>
2023-08-10 09:59:35 -0700

    Check

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,8 @@
  *
  * Return: Always 0 (Success)
  */
-int main( void)
+int main(void)
 {
-	puts("\"Programming is like building a multilingua puzzle\n");
+	puts( "\"Programming is like building a multilingua puzzle\n:anything" );
 	return(0);
 }
