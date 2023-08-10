74912bcf58ae2e9ecfb20dc50e1bdbdfd026e185
iadekunle3 <iadekunle34@gmail.com>
2023-08-10 06:43:20 -0700

    Put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * This is the code for printing my first C word
+ */
+int main()
+{
+	puts("Programming is like building a multilingua puzzle";
+			return (0);
+			
+			}
