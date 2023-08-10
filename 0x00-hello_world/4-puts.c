1965443f1396a3130db2922897c6d00423b934ee
iadekunle3 <iadekunle34@gmail.com>
2023-08-10 11:52:45 -0700

    Test

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-/** 
+/*
  * main - Entry point
- *
+ * Description: The main function is to start the program 
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingua puzzle\n, void");
-	return(0);
+	puts("\"Programming is like building a multilingua puzzle,\n");
+	return (0);
 }
