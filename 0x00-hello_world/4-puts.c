beb111cf3cd80e0cd79316b1a2cd0ddbcf0b833e
iadekunle3 <iadekunle34@gmail.com>
2023-08-10 09:38:22 -0700

    Commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-/*This is my first c programming language */
-int main()
+
+/* This is my first c programming language 
+ * main - Entry point
+ *
+ * return Always 0 (success)
+ * */
+int main(void)
 {
 	puts("\"Programming is like building a multilingua puzzle");
 	return(0);
