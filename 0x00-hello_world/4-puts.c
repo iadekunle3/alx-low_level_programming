e5c3c0304d12d2f31814c13af93f15d003fa4a4c
iadekunle3 <iadekunle34@gmail.com>
2023-08-10 07:08:02 -0700

    Check

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-
-int main(void){
-	puts("Programming is like building a multilingua puzzle,");
+/*This is my first c programming language */
+int main(void)
+{
+	puts ("\"Programming is like building a multilingua puzzle");
 	return(0);
 }
