9d6bc45ff06542d6508d6832f76ff75e12da7ece
iadekunle3 <iadekunle34@gmail.com>
2023-08-10 13:42:59 -0700

    Check

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - This is my entry point
+ *
+ * Description: This is the code for printing
+ * Return: Always return 0 if successful
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
