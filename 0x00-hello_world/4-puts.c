a8d7e631ab6abe2bcbc287616f38cd5a819c806e
iadekunle3 <iadekunle34@gmail.com>
2023-08-10 09:42:02 -0700

    Check

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-/* This is my first c programming language 
+/** 
  * main - Entry point
  *
- * return Always 0 (success)
- * */
+ * return Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingua puzzle");
