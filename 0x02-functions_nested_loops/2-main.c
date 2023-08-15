c768fe9df687aa8230838305e871f97788ebc3f8
iadekunle3 <iadekunle34@gmail.com>
2023-08-15 15:00:11 -0700

    check

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_alphabet - This is the main function
+ * Description: This is the breakdown
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_alphabet - This is the main function
+ * main - start here
+ * Description: This is to print ten times alphabet
+ * Return: Always as specified
+ */
+void print_alphabet(void)
+{
+	char ch;
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
