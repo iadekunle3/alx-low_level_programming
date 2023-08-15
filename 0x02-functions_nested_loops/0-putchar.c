5ebe1be6142817acac073dadd6dc5e0ee272056b
iadekunle3 <iadekunle34@gmail.com>
2023-08-15 03:17:31 -0700

    Check

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - This is a code to print putchar
+ * Description - For a putchar print
+ * Return: Always 0 when successful
+ */
+int main(void)
+{
+	printf("_putchar\n,");
+	return (0);
+}
