072342289489dbbbeba1c39843c098abff9fb8f5
iadekunle3 <iadekunle34@gmail.com>
2023-08-16 16:41:42 -0700

    check

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - this is an infinite loop
+ * Description: about the infinit loop
+ * Return: (0);
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("inifint loop incoming :(\n");
+
+	i = 0;
+
+	/**
+	 *while (i < 10)
+	 *{
+	 *putchar(i);
+	 }
+	 */
+	printf("infinit loop avoided! \\o/\n");
+	return (0);
+}
