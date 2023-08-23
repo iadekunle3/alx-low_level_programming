16508dc12ac09d05950c3fb27fa84e07e25aa3bf
iadekunle3 <iadekunle34@gmail.com>
2023-08-23 10:48:20 -0700

    check

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+#include <string.h>
 /**
  * _strncat - is the function to cat string
  * @dest: is the first defined integer
@@ -9,15 +9,5 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int x, y;
-
-	for (x = 0; dest[x] != '\0'; x++)
-	{
-		for (y = 0; src[y] != '\0' && y < n; y++)
-		{
-			dest[x + y] = src[y];
-			dest[x + y] = '\0';
-		}
-	}
-	return (dest);
+	return (strncat(dest, src, n));
 }
