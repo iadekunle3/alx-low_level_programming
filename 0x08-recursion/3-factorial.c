36729a8740853f024a510334f44b14cee6e4bbd2
iadekunle3 <iadekunle34@gmail.com>
2023-08-30 04:34:03 -0700

    check

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * factorial - is the function name
+ * @n: is the user input
+ * Return: as specified
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1);
+}
