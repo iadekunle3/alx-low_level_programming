98c11155ea3a24567355d3928589aec3d97f33c8
iadekunle3 <iadekunle34@gmail.com>
2023-08-30 09:34:17 -0700

    check

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,23 +1,30 @@
 #include "main.h"
-#include <math.h>
 /**
- * _sqrt - is the function to print sqr
- * @n: is the user input
- * Return: as specified
+ * sqrt_a - return the natural square root of
+ * a number
+ * @a: input number
+ * @b: iterator
+ * Return: square root or -1
  */
-int _sqrt(int n)
+int sqrt_a(int a, int b)
 {
-	if (n < 0)
+	if (b * b == a)
+	{
+		return (b);
+	}
+	else if (b * b > a)
 	{
 		return (-1);
 	}
-	int result = sqrt(n);
-	if (result * result == n)
-	{
-		return (result);
-	}
-	else
-	{
-		return (-1);
-	}
+	return (sqrt_a(a, b + 1));
+}
+/**
+ * _sqrt_recursion - returns the natural square root of
+ * a number
+ * @n: the input number
+ * Return: natural square root
+ */
+int _sqrt_recursion(int n)
+{
+	return (sqrt_a(n, 0));
 }
