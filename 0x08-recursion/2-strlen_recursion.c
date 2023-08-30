9937c054ac3b0f5b017b8a892ff3aac9fccc34d7
iadekunle3 <iadekunle34@gmail.com>
2023-08-30 04:25:43 -0700

    check

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strlen_recursion - is the string length
+ * @s: is the string pointer
+ * Return: as specified
+ */
+int _strlen_recursion(char *s)
+{
+	return strlen(s);
+}
