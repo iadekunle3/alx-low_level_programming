d9a6ff8faa38868208ee8087e30abd27c2f2f56a
iadekunle3 <iadekunle34@gmail.com>
2023-09-01 11:51:00 -0700

    check

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - is the function name
+ * @argc: is the arguement to main
+ * @argv: is the array
+ * Return: as specified
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2, sum;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	sum = num1 + num2;
+
+	printf("%d\n", sum);
+
+	return (0);
+}
