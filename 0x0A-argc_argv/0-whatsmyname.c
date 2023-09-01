c41e8ab04f98db8e9af306e2df13eff731da5e4e
iadekunle3 <iadekunle34@gmail.com>
2023-09-01 11:18:00 -0700

    check

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - is the function name
+ * @argc: is the argument to main
+ * @argv: is the argument array
+ * Return: as specified
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - is the function used
+ * @argc: is the argument to main for numbers
+ * @argv: is the first element
+ * Return: as specified
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
