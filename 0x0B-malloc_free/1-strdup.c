93292ab33041a3179f6e428146048da12e79b70c
iadekunle3 <iadekunle34@gmail.com>
2023-09-05 11:40:13 -0700

    strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _strdup - is the function that returns pointer
+ * @str: is the string used
+ * Return: as specified
+ */
+char *_strdup(char *str)
+{
+	char *new_str;
+	int i, len = 0;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (str[len])
+	{
+		len++;
+	}
+	new_str = malloc(sizeof(char) * (len + 1));
+	if (new_str == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < len; i++)
+	{
+		new_str[i] = str[i];
+	}
+	new_str[len] = '\0';
+
+	return (new_str);
+}
+
