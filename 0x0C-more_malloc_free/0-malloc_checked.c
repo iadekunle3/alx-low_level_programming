2d3ac5c0bbfb9a81619515bf6a6f03c84ef384b1
iadekunle3 <iadekunle34@gmail.com>
2023-09-07 09:35:01 -0700

    malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * malloc_checked - is the function to check memory address
+ * @b: is the integer argument used
+ * Return: as specified
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+	if (ptr == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(98);
+	}
+	return (ptr);
+}
