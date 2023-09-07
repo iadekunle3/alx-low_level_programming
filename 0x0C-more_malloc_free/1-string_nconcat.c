0a88f3bd2c1936be990b4bfa48e0473bf975fef5
iadekunle3 <iadekunle34@gmail.com>
2023-09-07 12:19:30 -0700

    malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,18 +14,26 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int len1, len2, i, j;
 
 	if (s1 == NULL)
+	{
 		s1 = "";
+	}
 	if (s2 == NULL)
-		s2 == "";
+	{
+		s2 = "";
+	}
 
 	len1 = strlen(s1);
-	len2 == strlen(s2);
+	len2 = strlen(s2);
 
 	if (n >= len2)
+	{
 		n = len2;
+	}
 	result = malloc(len1 + n + 1);
 	if (result == NULL)
+	{
 		return (NULL);
+	}
 
 	for (i = 0; i < len1; i++)
 		result[i]  = s1[i];
