c1b7b7df7922cd632ca9dc130ad5dfa0267e95f7
iadekunle3 <iadekunle34@gmail.com>
2023-09-11 10:53:14 -0700

    macro

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - is the function to print file
+ * Description: is the code that prints file name
+ * Return: as specified
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
