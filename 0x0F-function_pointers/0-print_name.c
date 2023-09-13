272eabfc53dbdecf49e9b0d8949bf1009e03e2b6
iadekunle3 <iadekunle34@gmail.com>
2023-09-13 03:47:27 -0700

    function pointer

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * print_name - is the function to print name
+ * @name: is the user input
+ * @f: is the pointer to the character
+ *
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
+/**
+ * print_function - is the function that print name as it is
+ * @name: is the user input
+ */
+void print_function(char *name)
+{
+	printf("%s\n", name);
+}
