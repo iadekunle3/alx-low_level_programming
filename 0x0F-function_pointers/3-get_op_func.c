6ac8974d34f4909c40109b1347a08d0ccc5a21ad
iadekunle3 <iadekunle34@gmail.com>
2023-09-13 12:18:14 -0700

    function

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,24 +1,25 @@
-#include <stdio.h>
-#include "3_calc.h"
-/* Here is your get_op_func function */
+#include <stddef.h>
+#include "3-calc.h"
+// Ensure these functions are defined somewhere in your code
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
 int (*get_op_func(char *s))(int, int)
 {
-    struct operator
-    {
-        char* op;
-        int (*f)(int, int);
-    };
-struct operator ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
     int i = 0;
+    char ops[] = "+-*/%";
+    int (*fns[])(int, int) = {op_add, op_sub, op_mul, op_div, op_mod};
 
-    while (ops[i].op != NULL && *(ops[i].op) != *s)
+    while (s[0] != ops[i] && i < 5)
         i++;
-    return (ops[i].f);
+
+    // Add a default return statement
+    if (i == 5)
+        return NULL;
+
+    return fns[i];
 }
+
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,25 +1,28 @@
-#include <stdio.h>
 #include <stdlib.h>
-
-int main(int argc, char *argv[]) {
+#include "3-calc.h"
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
     int num1, num2;
-    int (*op_func)(int, int);
+    int (*operation)(int, int);
 
-    if (argc != 4) {
+    if (argc != 4)
+    {
         printf("Usage: calc num1 operator num2\n");
         return (1);
     }
 
     num1 = atoi(argv[1]);
     num2 = atoi(argv[3]);
-    op_func = get_op_func(argv[2]);
+    operation = get_op_func(argv[2]);
 
-    if (op_func == NULL) {
-        printf("Invalid operator\n");
+    if (operation == NULL || (num2 == 0 && (*argv[2] == '/' || *argv[2] == '%')))
+    {
+        printf("Error\n");
         return (1);
     }
 
-    printf("%d\n", op_func(num1, num2));
+    printf("%d\n", operation(num1, num2));
     return (0);
 }
 
