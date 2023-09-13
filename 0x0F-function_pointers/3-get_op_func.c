9254fbf0249bfceb3217a717cbbd48038b059f6e
iadekunle3 <iadekunle34@gmail.com>
2023-09-13 12:42:24 -0700

    function pointers

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -6,25 +6,29 @@ int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
-
+/**
+ * get_op_func - is the function to get op function
+ * @s: is the string input
+ * Return: as specified
+ */
 int (*get_op_func(char *s))(int, int)
 {
-    op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i = 0;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
 
-    if (s == NULL)
-        return NULL;
+	if (s == NULL)
+		return (NULL);
 
-    while (ops[i].op != NULL && *(ops[i].op) != *s)
-        i++;
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
+		i++;
 
-    return (ops[i].f);
+	return (ops[i].f);
 }
 
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,28 +1,34 @@
 #include <stdlib.h>
 #include "3-calc.h"
 #include <stdio.h>
+/**
+ * main - is the function itself
+ * @argc: is the arguement count
+ * @argv: is the arguement array
+ * Return: as specified
+ */
 int main(int argc, char *argv[])
 {
-    int num1, num2;
-    int (*operation)(int, int);
+	int num1, num2;
+	int (*operation)(int, int);
 
-    if (argc != 4)
-    {
-        printf("Usage: calc num1 operator num2\n");
-        return (1);
-    }
+	if (argc != 4)
+	{
+		printf("Usage: calc num1 operator num2\n");
+		return (1);
+	}
 
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[3]);
-    operation = get_op_func(argv[2]);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	operation = get_op_func(argv[2]);
 
-    if (operation == NULL || (num2 == 0 && (*argv[2] == '/' || *argv[2] == '%')))
-    {
-        printf("Error\n");
-        return (1);
-    }
+	if (operation == NULL || (num2 == 0 && (*argv[2] == '/' || *argv[2] == '%')))
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-    printf("%d\n", operation(num1, num2));
-    return (0);
+	printf("%d\n", operation(num1, num2));
+	return (0);
 }
 
