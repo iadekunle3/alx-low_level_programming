f219ebbc568399b170204fa6de946492967d9a90
iadekunle3 <iadekunle34@gmail.com>
2023-09-13 13:18:58 -0700

    function pointers

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,41 +1,72 @@
+#include "3-calc.h"
 #include <stdio.h>
-#include "function_pointers.h"
-/* Returns the sum of a and b */
+
+/**
+ * op_add - add two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: 0 if successful. 1 otherwise.
+ */
+
 int op_add(int a, int b)
 {
-    return (a + b);
+	printf("%d\n", a + b);
+	return (0);
 }
 
-/* Returns the difference of a and b */
+/**
+ * op_sub - subtract two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: 0 if successful. 1 otherwise.
+ */
+
 int op_sub(int a, int b)
 {
-    return (a - b);
+	printf("%d\n", a - b);
+	return (0);
 }
 
-/* Returns the product of a and b */
+/**
+ * op_mul - multiply two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: 0 if successful. 1 otherwise.
+ */
+
 int op_mul(int a, int b)
 {
-    return (a * b);
+	printf("%d\n", a * b);
+	return (0);
 }
 
-/* Returns the result of the division of a by b */
+/**
+ * op_div - divides two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: 0 if successful. 1 otherwise.
+ */
+
 int op_div(int a, int b)
 {
-    if (b == 0)
-    {
-        printf("Error\n");
-        return (0);
-    }
-    return (a / b);
+	printf("%d\n", a / b);
+	return (0);
 }
 
-/* Returns the remainder of the division of a by b */
+/**
+ * op_mod - modulo of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: 0 if successful. 1 otherwise.
+ */
+
 int op_mod(int a, int b)
 {
-    if (b == 0)
-    {
-        printf("Error\n");
-        return (0);
-    }
-    return (a % b);
+	printf("%d\n", a % b);
+	return (0);
 }
