da605dcdd39c4389e58775740a3b66efb8046cea
iadekunle3 <iadekunle34@gmail.com>
2023-09-14 14:10:07 -0700

    functions

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,15 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+
+#include <stdarg.h>
+
+typedef struct print_d
+{
+	char *c;
+	void (*f_pr)(va_list a);
+} print_d;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
