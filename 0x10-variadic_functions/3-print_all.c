29561a41d237a128f1411e670021006c15412de5
iadekunle3 <iadekunle34@gmail.com>
2023-09-14 13:36:26 -0700

    print functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,45 +1,98 @@
+#include "variadic_functions.h"
 #include <stdarg.h>
 #include <stdio.h>
+
 /**
- * print_all - is a function that print all element
- * @format: is used to format
+ * print_char - function prints char
+ * @arg: argument
  */
-void print_all(const char * const format, ...)
+
+void print_char(va_list arg)
 {
-	va_list args;
-	char *str;
-	char *fmt = (char *)format;
-	char c;
+	char c = va_arg(arg, int);
 
-	va_start(args, format);
-
-	while (*fmt)
-	{
-		switch (*fmt++)
-		{
-			case 'c':
-			c = (char)va_arg(args, int);
-			printf("%c", c);
-			break;
-			case 'i':
-			printf("%d", va_arg(args, int));
-			break;
-			case 'f':
-			printf("%f", (float)va_arg(args, double));
-			break;
-			case 's':
-			str = va_arg(args, char*);
-			printf("%s", str ? str : "(nil)");
-			break;
-		}
-		if (*(fmt) && (*fmt == 'c' || *fmt == 'i' || *fmt == 'f' || *fmt == 's'))
-		{
-			printf(", ");
-		}
-	}
-
-	printf("\n");
-
-	va_end(args);
+	printf("%c", c);
 }
 
+/**
+ * print_int - function prints int
+ * @arg: argument
+ */
+
+void print_int(va_list arg)
+{
+	int i = va_arg(arg, int);
+
+	printf("%d", i);
+}
+
+/**
+ * print_float - function prints float
+ * @arg: argument
+ */
+
+void print_float(va_list arg)
+{
+	float f = va_arg(arg, double);
+
+	printf("%f", f);
+}
+
+/**
+ * print_string - function prints strings.
+ * @arg: argument
+ */
+
+void print_string(va_list arg)
+{
+	char *s = va_arg(arg, char *);
+
+	if (s == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", s);
+}
+
+/**
+ * print_all - function that prints anything.
+ * @format: format specifier
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list arg;
+	int i, j;
+	char *sep = "";
+
+	print_d data[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_string}
+	};
+
+	va_start(arg, format);
+
+	i = 0;
+	while (format && *(format + i))
+	{
+		j = 0;
+		while (j < 4 && *(format + i) != *(data[j].c))
+		{
+			j++;
+		}
+
+		if (j < 4)
+		{
+			printf("%s", sep);
+			data[j].f_pr(arg);
+			sep = ", ";
+		}
+		i++;
+	}
+	printf("\n");
+
+	va_end(arg);
+}
