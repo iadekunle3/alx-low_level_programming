2bb0a43a2a1a1da3596515b5d4479c3dcc353108
iadekunle3 <iadekunle34@gmail.com>
2023-09-21 13:44:43 -0700

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,7 +14,10 @@ size_t print_list(const list_t *h)
 	nodes = 0;
 	while (h != NULL)
 	{
-		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		nodes++;
 	}
