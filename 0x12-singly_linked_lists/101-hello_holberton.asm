ce5499c7bcf9a54c1ae3fdc3af2a763562d99876
iadekunle3 <iadekunle34@gmail.com>
2023-09-21 15:44:29 -0700

    print list

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,20 +1,12 @@
-section .data
-    hello db 'Hello, Holberton', 0
-
-section .text
-    global main
-    extern printf
+section		.text
+	extern	printf
+	global	main
 
 main:
-    ; Prepare arguments for printf
-    mov rdi, hello     ; format
-    xor rax, rax       ; vararg count
+	mov		edi, msg
+	mov		eax, 0
+	call	printf
 
-    ; Call printf
-    call printf
 
-    ; Return from main
-    mov eax, 0x60      ; syscall number (sys_exit)
-    xor edi, edi       ; exit code
-    syscall            ; call kernel
-
+section		.data
+	msg db 'Hello, Holberton', 0xa, 0
