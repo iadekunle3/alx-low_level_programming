a192b31555a3783edc31d1c222262ed1061fe4af
iadekunle3 <iadekunle34@gmail.com>
2023-09-21 08:53:43 -0700

    list lens

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,6 +1,7 @@
 #include "lists.h"
 #include <stdio.h>
 #include <stddef.h>
+#include <string.h>
 /**
  * print_list - prints all elements
  * @h: is the pointer to the list
@@ -20,8 +21,8 @@ size_t print_list(const list_t *h)
 		{
 			printf("[%d] %s\n", h->len, h->str);
 		}
-		nodes++;
 		h = h->next;
+		nodes++;
 	}
 	return (nodes);
 }
