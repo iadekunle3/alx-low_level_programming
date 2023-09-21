d668f2f78391af755f9a2bb89b3cfa2c22a76676
iadekunle3 <iadekunle34@gmail.com>
2023-09-21 09:09:05 -0700

    linked list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * add_node - is used to add nodes to the beginning of list
+ * @head: is the double pointer to list
+ * @str: is the string input
+ * Return: as specified
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *i;
+	int len = 0;
+
+	while (str[len])
+		len++;
+
+	i = malloc(sizeof(list_t));
+	if (!i)
+		return (NULL);
+
+	i->str = stedup(str);
+	if (!i->str)
+	{
+		free(i);
+		return (NULL);
+	}
+
+	i->len = len;
+	i->next = *head;
+	*head = i;
+
+	return (i);
+}
