a6997ea569fd86da60d76c27610f0a08ddebd7e2
iadekunle3 <iadekunle34@gmail.com>
2023-09-21 14:15:30 -0700

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,19 @@
 #include "lists.h"
 /**
+ * _strlen: is used to get str length
+ * @s: is the sstring pointer
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+	while (*s++)
+		i++;
+	return (i);
+}
+/**
  * print_list - prints all elements
  * @h: is the pointer to the list
  * Return: as specified
@@ -11,10 +25,7 @@ size_t print_list(const list_t *h)
 	i = 0;
 	while (h != NULL)
 	{
-		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
-		else
-			printf("[%d] %s\n", h->len, h->str);
+		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
 		h = h->next;
 		i++;
 	}
