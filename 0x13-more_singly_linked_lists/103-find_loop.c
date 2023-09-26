bf495fdc3568359ed232c3e5c47f5d15280b1695
iadekunle3 <iadekunle34@gmail.com>
2023-09-26 10:52:05 -0700

    linked list

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * find_listint_loop - is used to find loop in a list
+ * @head: is the pointer to the list
+ * Return: as specified
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *i, *j;
+
+	if (head == NULL)
+		return (NULL);
+
+	i = j = head;
+
+	while (i && j && j->next)
+	{
+		i = i->next;
+		j = j->next->next;
+
+		if (i == j)
+		{
+			i = head;
+			while (i != j)
+			{
+				i = i->next;
+				j = j->next;
+			}
+			return (i);
+		}
+	}
+	return (NULL);
+}
+
