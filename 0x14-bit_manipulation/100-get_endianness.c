d5391290653d51159e1f448b6e108e900f78a1ab
iadekunle3 <iadekunle34@gmail.com>
2023-09-28 14:33:15 -0700

    endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * get_endianness - is a function that create and read int byte
+ * Return: as specified
+ */
+int get_endianness(void)
+{
+	unsigned int i = 1;
+	char *c = (char*)&i;
+	return (int)*j;
+}
