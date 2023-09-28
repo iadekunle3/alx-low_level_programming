b5ba81536ead9e0e2f5aa560c95c8e6c74ffd694
iadekunle3 <iadekunle34@gmail.com>
2023-09-28 13:38:57 -0700

    get bit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * get_bit - is the function to get bit from given index
+ * @n: is the unsigned long int for user input
+ * @index: is the position of the input in the array
+ * Return: as specified
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index > (sizeof(n) *8))
+		return (-1);
+	return ((n >> index) & 1);
+}
