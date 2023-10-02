88e6f03957e9e563c14244bd31e36b88ca21bee4
iadekunle3 <iadekunle34@gmail.com>
2023-10-02 09:53:04 -0700

    elf header

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,7 +3,7 @@
 
 #include <stddef.h>
 #include <sys/types.h>
-
+#include <elf.h>
 /**
  * struct file - is the custom file for size
  * Always Return as specified
@@ -19,5 +19,6 @@ typedef struct file
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int main(int argc, char **argv);
 
 #endif
