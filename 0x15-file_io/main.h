91bfd79c83e1b43d002790c66cb8a5f29a29e541
iadekunle3 <iadekunle34@gmail.com>
2023-10-02 09:17:07 -0700

    read text file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stddef.h>
+#include <sys/types.h>
+
+typedef struct file
+{
+	size_t size;
+	ssize_t ssize;
+} file_t;
+
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
