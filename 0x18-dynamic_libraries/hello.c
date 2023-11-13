70fca7b6b8183dc7ae7bd8b7f96661aded0d427b
iadekunle3 <iadekunle34@gmail.com>
2023-11-13 08:05:45 -0800

    check

--- 0x18-dynamic_libraries/hello.c
+++ 0x18-dynamic_libraries/hello.c
@@ -1,9 +1,32 @@
 #include <stdio.h>
 
-void hello_world() {
-    printf("Hello, World!\n");
+int add(int a, int b) {
+	return a + b;
 }
 
-int add(int a, int b) {
-    return a + b;
+int sub(int a, int b) {
+	return a - b;
 }
+
+int mul(int a, int b) {
+	return a * b;
+}
+
+int div(int a, int b) {
+	if (b != 0) {
+		return a / b;
+	} else {
+		printf("Error: Division by zero is not allowed.\n");
+		return 0;
+	}
+}
+
+int mod(int a, int b) {
+	if (b != 0) {
+		return a % b;
+	} else {
+		printf("Error: Division by zero is not allowed.\n");
+		return 0;
+	}
+}
+
